df2e569ac8967a176d18bc7b44caf095bfcd9dae
mxjoly <maxime.joly2808@gmail.com>
2024-02-05 22:46:20 +0100

    update the pybind11 folder

--- module.cpp
+++ module.cpp
@@ -1,7 +1,7 @@
-#include "pybind11/include/pybind11/pybind11.h"
-#include "pybind11/include/pybind11/functional.h"
-#include "pybind11/include/pybind11/iostream.h"
-#include "pybind11/include/pybind11/stl.h"
+#include "pybind11/pybind11.h"
+#include "pybind11/functional.h"
+#include "pybind11/iostream.h"
+#include "pybind11/stl.h"
 #include <thread>
 #include "lib/config.h"
 #include "lib/population.h"
