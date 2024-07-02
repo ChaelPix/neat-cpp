ef0a7c0ca51dfe838283ff7f5643923af93afec9
Maxime Joly <maxime.joly2808@gmail.com>
2024-07-02 21:23:45 +0200

    modify the load function from population class

--- lib/population.cpp
+++ lib/population.cpp
@@ -368,13 +368,6 @@ void neat::Population::save(const std::string &filename)
             json["species"].push_back(s->to_json());
         }
 
-        // Save the genomes
-        json["genomes"] = nlohmann::json::array();
-        for (auto &g : this->genomes)
-        {
-            json["genomes"].push_back(g->to_json());
-        }
-
         std::ofstream file(filename);
         file << json.dump(4);
         file.close();
@@ -423,12 +416,13 @@ neat::Population *neat::Population::load(const std::string &filename, const Conf
             loadedPopulation->species.push_back(species);
         }
 
-        // Deserialize the genomes
-        nlohmann::json genomes_json = population_json["genomes"];
-        for (const auto &g : genomes_json)
+        // Add the genomes of the species to the population
+        for (auto &s : loadedPopulation->species)
         {
-            Genome *genome = Genome::from_json(g);
-            loadedPopulation->genomes.push_back(genome);
+            for (auto &g : s->genomes)
+            {
+                loadedPopulation->genomes.push_back(g);
+            }
         }
 
         std::cout << "Population loaded from '" << filename << "'" << std::endl;
